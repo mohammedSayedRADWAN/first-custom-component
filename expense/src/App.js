e14d33ef9403e7528cc2f32f5c2266eca99bede7
mohammedSayedRADWAN <github.com/mohammedsayedradwan>
2023-06-22 00:28:28 +0300

    use compostion children props

--- expense/src/App.js
+++ expense/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import '../src/components/ExpenseItem.css';
+import Expensee from "./components/Expense/Expensee";
+import "./components/Expenses.css";
 
-import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   const expenses = [
@@ -27,30 +28,9 @@ function App() {
   ];
   return (
     <div>
-      
-      <h2>Let's get started!</h2>
+            <h2>Let's get started!</h2>
 
-       <ExpenseItem
-       title={expenses[0].title}
-       amount={expenses[0].amount}
-       date={expenses[0].date}
-       ></ExpenseItem>
-       <ExpenseItem
-       title={expenses[1].title}
-       amount={expenses[1].amount}
-       date={expenses[1].date}
-       ></ExpenseItem>
-       <ExpenseItem
-       title={expenses[2].title}
-       amount={expenses[2].amount}
-       date={expenses[2].date}
-       ></ExpenseItem>
-       <ExpenseItem
-       title={expenses[3].title}
-       amount={expenses[3].amount}
-       date={expenses[3].date}
-       ></ExpenseItem>
-      
+      <Expensee data={expenses}/>
     </div>
   );
 }
